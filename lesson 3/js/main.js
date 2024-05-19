1ab71c3b4e3618f7c32d516600601cf5cd6542f0
Al Tsaplin <al.tsaplin@gmail.com>
2024-05-19 12:12:16 +0300

    add(notes & hws) For the lessons from 1 to 3

--- lesson 3/js/main.js
+++ lesson 3/js/main.js
@@ -3,8 +3,24 @@
 // $('a[download]').css('border', '1px solid red'); // Простой атрибут
 // $('a[href="http://ya.ru"]').css('border', '1px solid red'); // Полное соответствие
 // $('a[href!="http://ya.ru"]').css('border', '1px solid red'); // Полное несоответствие
-// $('a[href^="http"]').css('border', '1px solid red'); // Начинается с ...
+// $('a[href^="http"]').css('border', '1px solid red'); // Все, чей атрибут href начинается с ...
 // $('a[href$=".com"]').css('border', '1px solid red'); // Заканчивается на ... 
 // $('a[href*="google"]').css('border', '1px solid red'); // Содержит ... 
 // $('a[data-target|="main"]').css('border', '1px solid red'); // Имеет префикс - ... 
 // $('a[href][download]').css('border', '1px solid red'); // Имеет одновременно несколько атрибутов 
+// $('a[href], [download]').css('border', '1px solid red'); // Имеет один из атрибутов 
+
+/* 
+Написать на jQuery обращение к элементам по: 
+
+-> Атрибуту href 
+-> Атрибуту href, который полностью соответствует "http://mail.ru"
+-> Атрибуту src, который начинается на "img/"
+-> Атрибуту src, который заканчивается на ".js" 
+-> Атрибутам src и title  
+*/
+$('[href]');
+$('[href="http://mail.ru"]');
+$('[src^="img/"]');
+$('[src$=".js"]');
+$('[src], [title]');
